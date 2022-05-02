9c9aecd88aa998a898abc03e175b70dd93672cdd
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-05-02 01:20:47 -0300

    Add Player Health System

--- Tesis/Assets/Scripts/Gameplay/Characters/PlayerController.cs
+++ Tesis/Assets/Scripts/Gameplay/Characters/PlayerController.cs
@@ -2,12 +2,18 @@
 
 namespace TimeDistortion.Gameplay.Characters
 {
-    public class PlayerController : MonoBehaviour
+    public class PlayerController : MonoBehaviour, IHittable
     {
+        [Header("Cheat Values")]
+        [SerializeField] int damageToReceive;
+        [SerializeField] bool receiveDamageNow;
         [Header("Set Values")]
         [SerializeField] KinematicMoveController moveController;
+        [SerializeField] CharacterData data;
         [SerializeField] float rotationSpeed;
         [SerializeField] float jumpForce;
+        //[Header("Runtime Values")]
+        //[SerializeField] Stats currentStats;
 
         //Unity Events
         private void Start()
@@ -16,11 +22,18 @@ namespace TimeDistortion.Gameplay.Characters
             {
                 moveController = GetComponent<KinematicMoveController>();
             }
+
+            data.currentStats = data.baseStats;
         }
         private void Update()
         {
             Rotate();
             Move();
+            if (receiveDamageNow)
+            {
+                receiveDamageNow = false;
+                GetHitted(damageToReceive);
+            }
         }
 
         //Methods
@@ -42,6 +55,16 @@ namespace TimeDistortion.Gameplay.Characters
             {
                 moveController.publicMoveInput = moveInput;
             }
+        }
+
+        //Interface Implementations
+        public void GetHitted(int damage)
+        {
+            data.currentStats.health -= damage;
+
+            if (data.currentStats.health > 0) return;
+            data.currentStats.health = 0;
+            Destroy(gameObject); //replace for EVENT
         }
     }
 }
