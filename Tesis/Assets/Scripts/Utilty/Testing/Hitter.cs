bcb22464c2b6cb32b8d22eab740d2865e1c531b8
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-05-03 04:17:05 -0300

    Add Hit Tester

---
+++ Tesis/Assets/Scripts/Utilty/Testing/Hitter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace NAMESPACENAME
+{
+    public class Hitter : MonoBehaviour
+    {
+        [SerializeField] int damage;
+        [SerializeField] bool dealDamageNow;
+        IHittable iHittable;
+
+        //Unity Events
+        private void Start()
+        {
+            if (iHittable == null)
+            {
+                iHittable = GetComponent<IHittable>();
+            }
+        }
+        private void Update()
+        {
+            if (dealDamageNow)
+            {
+                dealDamageNow = false;
+                iHittable.GetHitted(damage);
+                Debug.Log("Hitted " + iHittable + " with " + damage + " raw damage");
+            }
+        }
+    }
+}
