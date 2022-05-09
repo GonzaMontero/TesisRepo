778cd5b22dfbf28e9a8c35dd2e0cafc66dba5d33
Gonzalo Montero <gonmon654@gmail.com>
2022-05-09 16:32:44 -0300

    Add Character Movement and Lock On Camera
    WIP:
    Lock On With Multiple Targets
    Jump
    Attack

---
+++ Tesis/Assets/Scripts/Gameplay/Movement/CameraHandler.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Handler
+{
+    public class CameraHandler : MonoBehaviour
+    {
+        InputHandler inputHandler;
+
+        public Transform targetTransform;
+        public Transform cameraTransform;
+        public Transform cameraPivotTransform;
+        private Transform myTransform;
+
+        private Vector3 cameraTransformPosition;
+        private LayerMask ignoreLayers;
+
+        private Vector3 cameraFollowVelocity = Vector3.zero;
+
+        public static CameraHandler singleton;
+
+        public float lookSpeed = 0.1f;
+        public float followSpeed = 0.1f;
+        public float pivotSpeed = 0.03f;
+
+        private float targetPosition;
+        private float defaultPosition;
+        private float lookAngle;
+        private float pivotAngle;
+
+        public float minimumPivot = -35;
+        public float maximumPivot = 35;
+
+        public float cameraSphereRadius = 0.2f;
+        public float cameraCollisionOffset = 0.2f;
+        public float minimumCollisionOffset = 0.2f;
+
+        public Transform currentLockOnTarget;
+
+        public float maximumLockOnDistance = 30;
+        List<CharacterManager> availableTargets = new List<CharacterManager>();
+        public Transform nearestLockOnTarget;
+
+        private void Awake()
+        {
+            singleton = this;
+            myTransform = transform;
+            defaultPosition = cameraTransform.localPosition.z;
+            ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
+
+            inputHandler = FindObjectOfType<InputHandler>();
+        }
+
+        public void FollowTarget(float delta)
+        {
+            Vector3 targetPosition = Vector3.SmoothDamp(myTransform.position, targetTransform.position, ref cameraFollowVelocity, delta / followSpeed);
+            myTransform.position = targetPosition;
+
+            HandleCameraCollisions(delta);
+        }
+
+        public void HandleCameraRotation(float delta, float mouseXInput, float mouseYInput)
+        {
+            if (!inputHandler.lockOnFlag && currentLockOnTarget==null)
+            {
+                lookAngle += (mouseXInput * lookSpeed) / delta;
+                pivotAngle -= (mouseYInput * pivotSpeed) / delta;
+                pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
+
+                Vector3 rotation = Vector3.zero;
+                rotation.y = lookAngle;
+                Quaternion targetRotation = Quaternion.Euler(rotation);
+                myTransform.rotation = targetRotation;
+
+                rotation = Vector3.zero;
+                rotation.x = pivotAngle;
+
+                targetRotation = Quaternion.Euler(rotation);
+                cameraPivotTransform.localRotation = targetRotation;
+            }
+            else
+            {
+                float velocity = 0;
+
+                cameraTransform.position = new Vector3(cameraTransform.position.x, 2.5f, cameraTransform.position.z);
+
+                Vector3 dir = currentLockOnTarget.position - transform.position;
+                dir.Normalize();
+                dir.y = 0;
+
+                Quaternion targetRotation = Quaternion.LookRotation(dir);
+                transform.rotation = targetRotation;
+
+                dir = currentLockOnTarget.position - cameraPivotTransform.position;
+                dir.Normalize();
+
+                targetRotation = Quaternion.LookRotation(dir);
+                Vector3 eulerAngles = targetRotation.eulerAngles;
+                eulerAngles.y = 0f;
+                cameraPivotTransform.localEulerAngles = eulerAngles;
+            }
+            
+        }
+
+        private void HandleCameraCollisions(float delta)
+        {
+            targetPosition = defaultPosition;
+            RaycastHit hit;
+            Vector3 direction = cameraTransform.position - cameraPivotTransform.position;
+            direction.Normalize();
+
+            if (Physics.SphereCast(cameraPivotTransform.position, cameraSphereRadius, direction, out hit, Mathf.Abs(targetPosition), ignoreLayers))
+            {
+                float dis = Vector3.Distance(cameraPivotTransform.position, hit.point);
+                targetPosition = -(dis - cameraCollisionOffset);
+            }
+
+            if (Mathf.Abs(targetPosition) < minimumCollisionOffset)
+            {
+                targetPosition = -minimumCollisionOffset;
+            }
+
+            cameraTransformPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, delta / 0.2f);
+            cameraTransform.localPosition = cameraTransformPosition;
+        }
+
+        public void HandleLockOn()
+        {
+            float shortestDistance = Mathf.Infinity;
+
+
+            Collider[] colliders = Physics.OverlapSphere(targetTransform.position, 26);
+
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                CharacterManager character = colliders[i].GetComponent<CharacterManager>();
+
+                if (character != null)
+                {
+                    Vector3 lockTargetDIrection = character.transform.position - targetTransform.position;
+                    float distanceFromTarget = Vector3.Distance(targetTransform.position,character.transform.position);
+                    float viewableAngle = Vector3.Angle(lockTargetDIrection, cameraTransform.forward);
+
+                    if (character.transform.root != targetTransform.transform.root && viewableAngle>-50 && viewableAngle < 60 && distanceFromTarget <= maximumLockOnDistance)
+                    {
+                        availableTargets.Add(character);
+                    }
+                }
+            }
+
+            for (int k = 0; k < availableTargets.Count; k++)
+            {
+                float distanceFromTarget=Vector3.Distance(targetTransform.position, availableTargets[k].transform.position);
+
+                if (distanceFromTarget < shortestDistance)
+                {
+                    shortestDistance = distanceFromTarget;
+                    nearestLockOnTarget = availableTargets[k].lockOnTransform;
+                }
+            }
+        }
+
+        public void ClearLockOnTargets()
+        {
+            availableTargets.Clear();
+            nearestLockOnTarget = null;
+            currentLockOnTarget = null;
+        }
+    }
+}
