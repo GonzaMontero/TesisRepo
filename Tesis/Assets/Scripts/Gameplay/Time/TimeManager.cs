ee9b12cae9562a34243a424a935fe4bc3c9a3e8a
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-05-15 21:18:35 -0300

    Add SlowMo Scope

--- Tesis/Assets/Scripts/Gameplay/Time/TimeManager.cs
+++ Tesis/Assets/Scripts/Gameplay/Time/TimeManager.cs
@@ -15,12 +15,14 @@ namespace TimeDistortion.Gameplay.Physic
             public Transform transform;
             public ITimed time;
             public float timer;
+            public int ID;
 
-            public SlowMoTarget(Transform newtransform, ITimed newTarget, float newTimer)
+            public SlowMoTarget(Transform newtransform, ITimed newTarget)
             {
                 transform = newtransform;
                 time = newTarget;
-                timer = newTimer;
+                timer = 0;
+                ID = transform.GetInstanceID();
             }
         }
 
@@ -36,16 +38,20 @@ namespace TimeDistortion.Gameplay.Physic
         [SerializeField] int maxTargets;
         [Header("Runtime Values")]
         [SerializeField] List<SlowMoTarget> targets;
+        [SerializeField] SlowMoTarget currentTarget;
         [SerializeField] Camera mainCam;
-        [SerializeField] Vector3 slowMoRayEnd;
         [SerializeField] Vector2 centerOfScreen;
         [SerializeField] float cooldownTimer;
         [SerializeField] bool slowMoIsReady;
         [SerializeField] bool playerOnFloor;
+        Dictionary<int, SlowMoTarget> targetIDs;
 
         public Action<bool> SlowMoReady;
+        public Action<bool> TargetInScope;
         public Action<Transform, float> ObjectSlowed;
         public Action<Transform> ObjectUnSlowed;
+
+        public float publicCooldownTimer { get { return cooldownTimer; } }
 
         //Unity Events
         private void Start()
@@ -55,9 +61,15 @@ namespace TimeDistortion.Gameplay.Physic
                 player = GameObject.FindGameObjectWithTag("Player").transform;
             }
 
+            //Set Time
             Time.timeScale = slowdownFactor;
+
+            //Set Camera
             mainCam = Camera.main;
             centerOfScreen = new Vector2(mainCam.pixelWidth / 2, mainCam.pixelHeight / 2);
+
+            //Instatiate Dictionary
+            targetIDs = new Dictionary<int, SlowMoTarget>();
         }
         void Update()
         {
@@ -70,10 +82,7 @@ namespace TimeDistortion.Gameplay.Physic
                 return;
             }
 
-            RaycastHit hit;
-            if (!Physics.Raycast(mainCam.ScreenPointToRay(centerOfScreen), out hit)) return;
-            slowMoRayEnd = hit.point;
-
+            GetValidTarget();
 
 #if UNITY_EDITOR
             DEBUGDrawRays();
@@ -88,13 +97,29 @@ namespace TimeDistortion.Gameplay.Physic
         public void OnInteract(InputAction.CallbackContext context)
         {
             if (!context.performed) return; //only do action on press frame
+            //Debug.Log("Key Pressed!");
+            if (currentTarget == null) return;
+            //Debug.Log("Target Is Valid!");
             if (!slowMoIsReady) return; //Only slow if slow mo mode is active
+            //Debug.Log("SlowMo Ready!");
             if (cooldownTimer < 1) return; //Only slow if cooldown over
+            //Debug.Log("Cooldown Over!");
+
+            //if target was already slowed, DEslow
+            if (targetIDs.ContainsKey(currentTarget.ID))
+            {
+                if (currentTarget.timer < 0) return; // return if already DeSlowing Target
+
+                //Debug.Log("DeSlowing " + currentTarget.transform + " by command!");
+                DeSlowTarget(currentTarget);
+                return;
+            }
 
             SlowTarget();
 
-            if (targets.Count >= maxTargets)
+            if (targets.Count > maxTargets)
             {
+                //Debug.Log("DeSlowing " + targets[0].transform + " by max reached!");
                 DeSlowTarget(targets[0]);
             }
         }
@@ -106,12 +131,12 @@ namespace TimeDistortion.Gameplay.Physic
 
             //Debug.DrawRay(mainCam.ScreenPointToRay(centerOfScreen).origin, mainCam.ScreenPointToRay(centerOfScreen).direction, Color.gray);
 
+            Vector3 direction = mainCam.ScreenPointToRay(centerOfScreen).direction;
             //Get target (if not, deactivate oldest target)
             RaycastHit hit;
-            Vector3 direction = (slowMoRayEnd - player.position).normalized;
-            if (!Physics.Raycast(player.position, direction, out hit, slowdownRange))
+            if (!Physics.Raycast(mainCam.ScreenPointToRay(centerOfScreen), out hit, slowdownRange))
             {
-                Debug.DrawRay(player.position, direction * slowdownRange, Color.blue);
+                Debug.DrawRay(mainCam.transform.position, direction * slowdownRange, Color.blue);
                 //Debug.Log("Hitted Nothing");
                 return;
             }
@@ -120,12 +145,12 @@ namespace TimeDistortion.Gameplay.Physic
             ITimed target = hit.transform.GetComponent<ITimed>();
             if (target == null)
             {
-                Debug.DrawRay(player.position, direction * slowdownRange, Color.red);
+                Debug.DrawRay(mainCam.transform.position, direction * slowdownRange, Color.red);
                 //Debug.Log("Hitted Not Valid");
             }
             else
             {
-                Debug.DrawRay(player.position, direction * slowdownRange, Color.green);
+                Debug.DrawRay(mainCam.transform.position, direction * slowdownRange, Color.green);
                 //Debug.Log("Hitted Valid");
             }
         }
@@ -136,51 +161,58 @@ namespace TimeDistortion.Gameplay.Physic
 
             Debug.Log("SlowMo Ready: " + slowMoIsReady);
         }
-        void SlowTarget()
+        void GetValidTarget()
         {
-            //Get target (if not, deactivate oldest target)
+            if (!slowMoIsReady) return; //Only slow if slow mo mode is active
+           
+            //Get target (if not, exit)
             RaycastHit hit;
-            Physics.Raycast(player.position, player.forward, out hit, slowdownRange);
+            Physics.Raycast(mainCam.ScreenPointToRay(centerOfScreen), out hit, slowdownRange);
             if (hit.transform == null)
             {
-                if (targets.Count > 0)
-                {
-                    StartCoroutine(DeSlowTarget(targets[0]));
-                }
+                currentTarget = null;
+                TargetInScope?.Invoke(false);
                 return;
             }
 
-            //Check if target is valid (if not, deactivate oldest target)
+            //Check if target is valid (if not, exit)
             ITimed objectToSlow = hit.transform.GetComponent<ITimed>();
             if (objectToSlow == null)
             {
-                if (targets.Count > 0)
-                {
-                    StartCoroutine(DeSlowTarget(targets[0]));
-                }
+                currentTarget = null;
+                TargetInScope?.Invoke(false);
                 return;
             }
 
+            //Debug.Log("Valid Target");
+
+            //If already targetting a valid object, exit
+            if (currentTarget != null) return;
+            
+            //Debug.Log("Target Found!");
+            currentTarget = new SlowMoTarget(hit.transform, objectToSlow);
+            TargetInScope?.Invoke(true);
+        }
+        void SlowTarget()
+        {
             //Add target to list and slow it
-            targets.Add(new SlowMoTarget(hit.transform, objectToSlow, 0));
-            objectToSlow.TimeChanged(true);
-            ObjectSlowed?.Invoke(hit.transform, slowdownLength + slowdownExtraLength);
+            targets.Add(currentTarget);
+            targetIDs.Add(currentTarget.ID, currentTarget);
+            currentTarget.time.TimeChanged(true);
+            ObjectSlowed?.Invoke(currentTarget.transform, slowdownLength + slowdownExtraLength);
 
             //Start Cooldown
             cooldownTimer = 0;
         }
-        IEnumerator DeSlowTarget(SlowMoTarget target)
+        void DeSlowTarget(SlowMoTarget target)
         {
             //Update List
             targets.Remove(target);
-            Debug.Log("Target Removed at " + Time.realtimeSinceStartup + "!");
 
-            yield return new WaitForSecondsRealtime(slowdownExtraLength);
+            //Update Timer so dictionary doesn't break
+            target.timer = -1f;
 
-            //Update Target Time
-            Debug.Log("Target UnSlowed at " + Time.realtimeSinceStartup + "!");
-            target.time.TimeChanged(false);
-            ObjectUnSlowed?.Invoke(target.transform);
+            StartCoroutine(DeSlowRoutine(target));
         }
         void UpdateTimers()
         {
@@ -191,17 +223,20 @@ namespace TimeDistortion.Gameplay.Physic
             }
 
             //Run SlowMo Timers from targets
-            if (targets.Count <= int.MinValue) return;
+            if (targets.Count < 1) return;
             int i = 0;
             while (i < targets.Count)
             {
                 if (targets[i].timer > 1)
                 {
-                    StartCoroutine(DeSlowTarget(targets[i]));                    
+                    //Debug.Break();
+                    //Debug.Log("DeSlowing " + targets[i].transform + " by time reached!");
+                    DeSlowTarget(targets[i]);
                     continue;
                 }
 
                 targets[i].timer += Time.unscaledDeltaTime / slowdownLength;
+                //Debug.Log(targets[i] + " timer: " + targets[i].timer);
                 i++;
             }
         }
@@ -217,6 +252,22 @@ namespace TimeDistortion.Gameplay.Physic
 #endif
 
             return player;
+        }
+
+        //Routines
+        IEnumerator DeSlowRoutine(SlowMoTarget target)
+        {
+            //Debug.Log("Target Unslowing at " + Time.realtimeSinceStartup + "!");
+
+            yield return new WaitForSecondsRealtime(slowdownExtraLength);
+
+            //Update Dictionary
+            targetIDs.Remove(target.ID);
+
+            //Update Target Time
+            //Debug.Log("Target UnSlowed at " + Time.realtimeSinceStartup + "!");
+            target.time.TimeChanged(false);
+            ObjectUnSlowed?.Invoke(target.transform);
         }
     }
 }
