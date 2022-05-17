29515890577af1d3c3bd15ce5a4cbf8c24253b45
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-05-17 03:10:26 -0300

    Remove old PlayerController MoveInput

--- Tesis/Assets/Scripts/Gameplay/Characters/PlayerController.cs
+++ Tesis/Assets/Scripts/Gameplay/Characters/PlayerController.cs
@@ -6,10 +6,7 @@ namespace TimeDistortion.Gameplay.Characters
     public class PlayerController : MonoBehaviour, IHittable
     {
         [Header("Set Values")]
-        [SerializeField] KinematicMoveController moveController;
         [SerializeField] CharacterData data;
-        [SerializeField] float rotationSpeed;
-        [SerializeField] float jumpForce;
         //[Header("Runtime Values")]
         //[SerializeField] Stats currentStats;
 
@@ -23,40 +20,8 @@ namespace TimeDistortion.Gameplay.Characters
         {
             data.currentStats = data.baseStats;
         }
-        private void Start()
-        {
-            if (!moveController)
-            {
-                moveController = GetComponent<KinematicMoveController>();
-            }
-
-        }
-        private void Update()
-        {
-            Rotate();
-            Move();
-        }
 
         //Methods
-        private void Rotate()
-        {
-            float scaledRotationSpeed = rotationSpeed * Camera.main.scaledPixelWidth;
-            float mouseRotation = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-            transform.Rotate(0, mouseRotation, 0, Space.World);
-        }
-        void Move()
-        {
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-            float y = Input.GetKey(KeyCode.Space) && moveController.inFloor ? 1 : 0;
-            
-            Vector3 moveInput = new Vector3(x, y * jumpForce, z);
-
-            if (moveInput.sqrMagnitude > 0)
-            {
-                moveController.publicMoveInput = moveInput;
-            }
-        }
 
         //Interface Implementations
         public void GetHitted(int damage)
