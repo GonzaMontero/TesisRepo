6ac9a7b8528d45267650d906be231ebf990aac7e
Gonzalo Montero <gonmon654@gmail.com>
2022-05-18 20:51:01 -0300

    Add Gonza Testing and Sword Script

---
+++ Tesis/Assets/Scripts/Gameplay/Characters/SwordHit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class SwordHit : MonoBehaviour
+{
+    [SerializeField] BoxCollider hitRange;
+    [SerializeField] int damage;
+
+
+    private void Awake()
+    {
+        hitRange = GetComponent<BoxCollider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (gameObject.transform.tag == "Player")
+            return;
+
+        IHittable hit;
+        hit = collision.gameObject.GetComponent<IHittable>();
+        if (hit != null)
+        {
+            hit.GetHitted(damage);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (gameObject.transform.tag == "Player")
+            return;
+
+        IHittable hit;
+        hit = collision.gameObject.GetComponent<IHittable>();
+        if (hit != null)
+        {
+            hit.GetHitted(damage);
+        }
+    }
+}
