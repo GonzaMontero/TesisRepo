235da0215842bb0f07b85d48b7c09a0dfe26b4db
MikePineapple <miguel.pignatiello@gmail.com>
2022-05-18 19:07:21 -0300

    Camera Fixing

--- Tesis/Assets/Scripts/Gameplay/Movement/CameraHandler.cs
+++ Tesis/Assets/Scripts/Gameplay/Movement/CameraHandler.cs
@@ -130,7 +130,7 @@ namespace TimeDistortion.Gameplay.Handler
             }
 
             cameraTransformPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, delta / 0.2f);
-            cameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, Yvalue, cameraTransform.localPosition.z);
+            cameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, Yvalue, cameraTransformPosition.z);
         }
 
         public void HandleLockOn()
