b38e5db1c7785bca176241f800b2683fdd5b916f
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-05-20 13:33:01 -0300

    Add CircuitPart Model Controller
    AKA Target Animation triggerer

---
+++ Tesis/Assets/Scripts/Gameplay/Props/Circuit/CircuitPartModel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace TimeDistortion.Gameplay.Props
+{
+    public class CircuitPartModel : MonoBehaviour
+    {
+        [Header("Set Values")]
+        [SerializeField] CircuitPartController controller;
+        [SerializeField] Animator animator;
+        [SerializeField] string animationTrigger;
+
+        //Unity Events
+        private void Start()
+        {
+            if (controller == null)
+            {
+                controller = GetComponent<CircuitPartController>();
+            }
+            if (animator == null)
+            {
+                animator = GetComponent<Animator>();
+            }
+
+            controller.Activated += OnControllerActivated;
+        }
+
+        //Methods
+
+        //Event Receivers
+        void OnControllerActivated(CircuitPartController controller)
+        {
+            animator.SetTrigger(animationTrigger);
+        }
+    }
+}
