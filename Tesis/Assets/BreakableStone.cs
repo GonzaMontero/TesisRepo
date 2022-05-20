31bbcf4f25249d2adc9db9042a9a304ca3ad4c93
MikePineapple <miguel.pignatiello@gmail.com>
2022-05-20 16:55:26 -0300

    Sword Hit Available

---
+++ Tesis/Assets/BreakableStone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TimeDistortion.Gameplay.Props
+{
+    public class BreakableStone : MonoBehaviour, IHittable
+    {
+        public void GetHitted(int damage)
+        { Debug.Log("hIT!"); }
+    }
+}
