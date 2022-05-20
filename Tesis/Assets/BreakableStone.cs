a868f4f072df7cdd73514dbc58de3a8c339efb78
Gonzalo Montero <gonmon654@gmail.com>
2022-05-20 17:42:05 -0300

    Add Prefab For Rock

--- Tesis/Assets/BreakableStone.cs
+++ Tesis/Assets/BreakableStone.cs
@@ -5,7 +5,13 @@ namespace TimeDistortion.Gameplay.Props
 {
     public class BreakableStone : MonoBehaviour, IHittable
     {
+        [SerializeField] GameObject brokenRock;
+        [SerializeField] Transform floorLevel;
+
         public void GetHitted(int damage)
-        { Debug.Log("hIT!"); }
+        {
+            Instantiate(brokenRock, floorLevel.position, Quaternion.identity, this.transform.parent);
+            Destroy(this.gameObject);
+        }
     }
 }
