58f79cd68635f6819eb85ff60ea5ad82ccebd633
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-06-23 00:17:44 -0300

    Add Player Audio

---
+++ Tesis/Assets/Scripts/Gameplay/Characters/PlayerAudio.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+
+namespace TimeDistortion.Gameplay.Characters
+{
+    public class PlayerAudio : MonoBehaviour
+    {
+        [Header("Set Values")]
+        [SerializeField] Handler.InputHandler controller;
+        [SerializeField] Physic.TimeManager timeManager;
+        [SerializeField] FMODUnity.EventReference walkAudio;
+        [SerializeField] FMODUnity.EventReference jumpAudio;
+        [SerializeField] FMODUnity.EventReference landAudio;
+        [SerializeField] FMODUnity.EventReference attackAudio;
+        [SerializeField] FMODUnity.EventReference hitAudio;
+        [SerializeField] FMODUnity.EventReference setSlowMoAudio;
+        [SerializeField] FMODUnity.EventReference activateSlowMoAudio;
+        [SerializeField] FMODUnity.EventReference slowMoAudio;
+        [SerializeField] FMODUnity.EventReference slowMoFailedAudio;
+        [Header("Runtime Values")]
+        [SerializeField] bool playerOnAir;
+        [SerializeField] bool playerWalking;
+        [SerializeField] bool slowMoReady;
+        FMOD.Studio.EventInstance walkAudioInstance;
+        FMOD.Studio.EventInstance slowMoReadyAudioInstance;
+
+        //Unity Events
+
+        private void Start()
+        {
+            if (!timeManager)
+            {
+                timeManager = Physic.TimeManager.Get();
+            }
+            if (!controller)
+            {
+                controller = GetComponent<Handler.InputHandler>();
+            }
+            
+            //controller.PlayerAttacked += OnPlayerAttacked;
+            controller.PlayerJumped += OnPlayerJumped;
+            controller.PlayerMoved += OnPlayerMoved;
+            timeManager.SlowMoReady += OnSlowMoReady;
+            timeManager.ObjectSlowed += OnObjectSlowed;
+            //timeManager.ObjectUnSlowed +=;
+        }
+        private void Update()
+        {
+            if (controller.publicGroundedPlayer != playerOnAir) return;
+            playerOnAir = !controller.publicGroundedPlayer;
+            
+            if (playerOnAir) return;
+            OnPlayerLanded();
+        }
+
+
+        //Event Receivers
+        void OnPlayerAttacked()
+        {
+            //FMODUnity.RuntimeManager.PlayOneShot(attackAudio);
+        }
+        void OnPlayerJumped()
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(jumpAudio);
+        }
+        void OnPlayerLanded()
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(landAudio);
+        }
+        void OnPlayerMoved(bool playerMoved)
+        {
+            //If already using right audio, exit
+            if (playerWalking == playerMoved) return;
+
+            //If moving play loop, else stop
+            if (playerMoved)
+            {
+                walkAudioInstance = FMODUnity.RuntimeManager.CreateInstance(walkAudio);
+                walkAudioInstance.start();
+            }
+            else
+            {
+                walkAudioInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                walkAudioInstance.release();
+            }
+
+            //Update value
+            playerWalking = playerMoved;
+        }
+        void OnSlowMoReady(bool isReady)
+        {
+            //If already using right audio, exit
+            if (slowMoReady == isReady) return;
+
+            //If moving play loop, else stop
+            if (isReady)
+            {
+                slowMoReadyAudioInstance = FMODUnity.RuntimeManager.CreateInstance(setSlowMoAudio);
+                slowMoReadyAudioInstance.start();
+            }
+            else
+            {
+                slowMoReadyAudioInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                slowMoReadyAudioInstance.release();
+            }
+
+            //Update value
+            slowMoReady = isReady;
+        }
+        void OnObjectSlowed(Transform notUsed, float _notUsed)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(activateSlowMoAudio);
+        }
+    }
+}
