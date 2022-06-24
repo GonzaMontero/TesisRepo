0d5f778de78d7e3804561419354dfae8e34608dc
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-06-24 19:21:58 -0300

    Add Timerock, Door & Target Sounds

---
+++ Tesis/Assets/Scripts/Gameplay/Props/Circuit/CircuitManagerAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace TimeDistortion.Gameplay.Props
+{
+    public class CircuitManagerAudio : MonoBehaviour
+    {
+        [Header("Set Values")]
+        [SerializeField] CircuitManager controller;
+        [SerializeField] FMODUnity.EventReference completedAudio;
+
+        //Unity Events
+        private void Start()
+        {
+            if (!controller)
+            {
+                controller = GetComponent<CircuitManager>();
+            }
+
+            controller.CircuitCompleted += OnCompleted;
+        }
+
+        //Event Receivers
+        void OnCompleted()
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(completedAudio);
+        }
+    }
+}
---
+++ Tesis/Assets/Scripts/Gameplay/Props/Circuit/CircuitPartAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace TimeDistortion.Gameplay.Props
+{
+    public class CircuitPartAudio : MonoBehaviour
+    {
+        [Header("Set Values")]
+        [SerializeField] CircuitPartController controller;
+        [SerializeField] FMODUnity.EventReference activateAudio;
+
+        //Unity Events
+        private void Start()
+        {
+            if (!controller)
+            {
+                controller = GetComponent<CircuitPartController>();
+            }
+
+            controller.Activated += OnActivated;
+        }
+
+        //Event Receivers
+        void OnActivated(CircuitPartController notUsed)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(activateAudio);
+        }
+    }
+}
