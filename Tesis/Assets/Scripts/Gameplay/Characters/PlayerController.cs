f054cdff99de4e396d2b9eb6c28ccfd3014d94f6
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-10-05 22:18:16 -0300

    Add Player stair step climb

--- Tesis/Assets/Scripts/Gameplay/Characters/PlayerController.cs
+++ Tesis/Assets/Scripts/Gameplay/Characters/PlayerController.cs
@@ -29,6 +29,11 @@ namespace TimeDistortion.Gameplay.Handler
         private float mouseY;
         private bool jumpInput;
 
+        [SerializeField] GameObject stepRayUpper;
+        [SerializeField] GameObject stepRayLower;
+        [SerializeField] float stepHeight = 0.3f;
+        [SerializeField] float stepSmooth = 2f;
+        
         public bool lockOnFlag { set; private get; }
 
         #endregion
@@ -126,6 +131,8 @@ namespace TimeDistortion.Gameplay.Handler
                 coll = GetComponent<CapsuleCollider>();
             }
             InitRigidSystem();
+            
+            stepRayUpper.transform.position = new Vector3(stepRayUpper.transform.position.x, stepHeight, stepRayUpper.transform.position.z);
         }
 
         private void InitRigidSystem()
@@ -178,6 +185,11 @@ namespace TimeDistortion.Gameplay.Handler
         private void LateUpdate()
         {
             jumpInput = false;
+        }
+        
+        private void FixedUpdate()
+        {
+            StepClimb();
         }
 
 #if UNITY_EDITOR
@@ -422,6 +434,45 @@ namespace TimeDistortion.Gameplay.Handler
             if (grounded) return;
             if(collision.gameObject.CompareTag("Ground")) return;
             OnAir();
+        }
+
+        void StepClimb()
+        {
+            RaycastHit hitLower;
+            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward),
+                    out hitLower, 0.1f))
+            {
+                RaycastHit hitUpper;
+                if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward),
+                        out hitUpper, 0.2f))
+                {
+                    transform.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+                }
+            }
+
+            RaycastHit hitLower45;
+            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1),
+                    out hitLower45, 0.1f))
+            {
+                RaycastHit hitUpper45;
+                if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f, 0, 1),
+                        out hitUpper45, 0.2f))
+                {
+                    transform.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+                }
+            }
+
+            RaycastHit hitLowerMinus45;
+            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1),
+                    out hitLowerMinus45, 0.1f))
+            {
+                RaycastHit hitUpperMinus45;
+                if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1),
+                        out hitUpperMinus45, 0.2f))
+                {
+                    transform.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
+                }
+            }
         }
 
         #endregion
