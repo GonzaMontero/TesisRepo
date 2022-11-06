a65ddecff9cdcfa726d3fdd873f816ddaa24fdf5
Alejandro Insausti <alejandroninsausti@gmail.com>
2022-11-06 04:09:29 -0300

    Rework door
    Updated prefab, abilitated manual set on door positions and added gizmos

--- Tesis/Assets/Scripts/Gameplay/Props/Circuit/Door/DoorCircuitController.cs
+++ Tesis/Assets/Scripts/Gameplay/Props/Circuit/Door/DoorCircuitController.cs
@@ -14,43 +14,59 @@ namespace TimeDistortion.Gameplay.Props.Circuit
         [Header("Runtime Values")]
         [SerializeField] Vector3 targetPos;
         [SerializeField] Vector3 moveDir;
+        [Header("DEBUG")]
+        [SerializeField] bool setPositions;
 
         //Unity Events
         private void Start()
         {
             manager.CircuitCompleted += OnCircuitCompleted;
-            SetPositions();
         }
         void Update()
         {
             Move();
         }
+#if UNITY_EDITOR
+        void OnDrawGizmos()
+        {
+            //Draw closed door
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube( transform.parent.position + closedPos, coll.bounds.size);
+
+            //Draw opened door
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(transform.parent.position + openedPos, coll.bounds.size);
+
+            if(!setPositions) return;
+            setPositions = false;
+            SetPositions();
+        }
+#endif
 
         //Methods
         void SetPositions()
         {
-            closedPos = transform.position;
-            openedPos = transform.position + coll.bounds.size.y * transform.up;
+            closedPos = transform.localPosition;
+            openedPos = transform.localPosition + coll.bounds.size.y * transform.up;
         }
         void Move()
         {
             if(moveDir.sqrMagnitude == 0) return;
 
             Vector3 movement = moveDir * speed * Time.deltaTime;
+            Vector3 pos = transform.localPosition;
 
-            float disToTarget = Vector3.Distance(transform.position, targetPos);
-            float disToMove = Vector3.Distance(transform.position, transform.position + movement);
+            float disToTarget = Vector3.Distance(pos, targetPos);
+            float disToMove = Vector3.Distance(pos, pos + movement);
             
             if (disToTarget < disToMove)
             {
                 moveDir = Vector3.zero;
-                transform.position = targetPos;
-                Debug.Log("Reached Destiny!");
+                transform.localPosition = targetPos;
                 return;
             }
             
             transform.Translate(movement);
-
         }
 
         //Event Receiver
@@ -65,7 +81,7 @@ namespace TimeDistortion.Gameplay.Props.Circuit
                 targetPos = closedPos;
             }
 
-            moveDir = (targetPos - transform.position).normalized;
+            moveDir = (targetPos - transform.localPosition).normalized;
         }
     }
 }
