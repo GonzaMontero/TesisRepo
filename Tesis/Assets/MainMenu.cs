e3847f2731aff49692a03fd7d5c5eaefcccec3ed
MikePineapple <miguel.pignatiello@gmail.com>
2022-11-10 06:40:16 -0300

    UI General Polish

--- Tesis/Assets/MainMenu.cs
+++ Tesis/Assets/MainMenu.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play()
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public void Play(int sceneIndex)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadAsynchronously(sceneIndex));
     }
 
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
     public void Options()
     {
 
