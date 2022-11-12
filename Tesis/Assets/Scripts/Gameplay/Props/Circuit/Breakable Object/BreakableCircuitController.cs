e388caab26596e3047c1ee2f450562f67c4b28c5
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-11-11 22:31:48 -0300

    Add BreakableCircuitPart

---
+++ Tesis/Assets/Scripts/Gameplay/Props/Circuit/Breakable Object/BreakableCircuitController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace TimeDistortion.Gameplay.Props.Circuit
+{
+    public class BreakableCircuitController : MonoBehaviour
+    {
+        [Header("Set Values")]
+        [SerializeField] CircuitManager manager;
+        //[SerializeField] Rigidbody[] breakableParts;
+        //[SerializeField] float explosionImpulse;
+        //[Header("Runtime Values")]
+
+
+        //Unity Events
+        void Start()
+        {
+            manager.CircuitCompleted += OnCircuitCompleted;
+        }
+
+        //Methods
+        void Break()
+        {
+            Destroy(gameObject);
+            // for (int i = 0; i < breakableParts.Length; i++)
+            // {
+            //     breakableParts[i].isKinematic = false;
+            //     breakableParts[i].AddExplosionForce(explosionImpulse, transform.position, 5f);
+            // }
+        }
+        
+        //Event Receiver
+        void OnCircuitCompleted(bool isCircuitComplete)
+        {
+            if (isCircuitComplete)
+            {
+                Break();
+            }
+        }
+    }
+}
