b57ed37751e34cb713375e31280351fdb94e3e98
Alejandro N. Insausti <alejandroninsausti@gmail.com>
2022-12-10 23:19:30 -0300

    Add VFX to Hazards
    (using Hazard Model)

---
+++ Tesis/Assets/Scripts/Gameplay/Props/Hazard/HazardController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TimeDistortion.Gameplay.Props
+{
+    public class HazardController : MonoBehaviour
+    {
+        [SerializeField] int damage;
+
+        public System.Action<Collision> HittedSomething;
+        
+        private void OnCollisionStay(Collision collision)
+        {
+            IHittable hittedObject = collision.gameObject.GetComponent<IHittable>();
+            
+            if (hittedObject == null) return;
+            
+            hittedObject.GetHitted(damage);
+            HittedSomething?.Invoke(collision);
+        }
+    }
+}
